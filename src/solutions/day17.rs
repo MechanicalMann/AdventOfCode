f39e0e6c664eb0bb04168d91ce57966b86902b14
Michael Mechmann <malorisdead@gmail.com>
2020-12-18 00:23:43 -0800

    Day 17 part 2 done.

--- src/solutions/day17.rs
+++ src/solutions/day17.rs
@@ -9,6 +9,7 @@ enum Cube {
 
 #[derive(Clone, Copy, Debug)]
 struct Coordinates {
+    w: isize,
     x: isize,
     y: isize,
     z: isize,
@@ -16,29 +17,34 @@ struct Coordinates {
 
 struct CubeUpdate {
     cube: Cube,
-    coordinates: Coordinates, // z, y, x
+    coordinates: Coordinates,
 }
 
 struct PocketDimension {
-    cubes: Vec<Vec<Vec<Cube>>>, // What's our vector, Victor?
+    cubes: Vec<Vec<Vec<Vec<Cube>>>>, // What's our vector, Victor?
     offsets: Coordinates,
 }
 impl PocketDimension {
-    fn shift(&self, coordinates: &Coordinates) -> (usize, usize, usize) {
+    fn shift(&self, coordinates: &Coordinates) -> (usize, usize, usize, usize) {
+        let w = (coordinates.w + self.offsets.w) as usize;
         let x = (coordinates.x + self.offsets.x) as usize;
         let y = (coordinates.y + self.offsets.y) as usize;
         let z = (coordinates.z + self.offsets.z) as usize;
-        (x, y, z)
+        (w, x, y, z)
     }
 
     fn get_cube(&self, at: &Coordinates) -> Cube {
-        let (x, y, z) = self.shift(at);
+        let (w, x, y, z) = self.shift(at);
 
-        if z >= self.cubes.len() || y >= self.cubes[z].len() || x >= self.cubes[z][y].len() {
+        if z >= self.cubes.len()
+            || y >= self.cubes[z].len()
+            || x >= self.cubes[z][y].len()
+            || w >= self.cubes[z][y][x].len()
+        {
             return Cube::Inactive;
         }
 
-        return self.cubes[z][y][x];
+        return self.cubes[z][y][x][w];
     }
 
     fn get_change(&self, origin: &Coordinates) -> Option<Cube> {
@@ -46,17 +52,20 @@ impl PocketDimension {
         let zs = [origin.z - 1, origin.z, origin.z + 1];
         let ys = [origin.y - 1, origin.y, origin.y + 1];
         let xs = [origin.x - 1, origin.x, origin.x + 1];
+        let ws = [origin.w - 1, origin.w, origin.w + 1];
 
         let mut active = 0;
         for z in zs.iter().cloned() {
             for y in ys.iter().cloned() {
                 for x in xs.iter().cloned() {
-                    if z == origin.z && y == origin.y && x == origin.x {
-                        continue;
-                    }
-                    let search = Coordinates { x, y, z };
-                    if matches!(self.get_cube(&search), Cube::Active) {
-                        active += 1;
+                    for w in ws.iter().cloned() {
+                        if z == origin.z && y == origin.y && x == origin.x && w == origin.w {
+                            continue;
+                        }
+                        let search = Coordinates { w, x, y, z };
+                        if matches!(self.get_cube(&search), Cube::Active) {
+                            active += 1;
+                        }
                     }
                 }
             }
@@ -79,9 +88,12 @@ impl PocketDimension {
                 let y = y as isize - 1;
                 for x in 0..self.cubes[0][0].len() + 2 {
                     let x = x as isize - 1;
-                    coordinates = Coordinates { x, y, z };
-                    if let Some(cube) = self.get_change(&coordinates) {
-                        changes.push(CubeUpdate { coordinates, cube })
+                    for w in 0..self.cubes[0][0][0].len() + 2 {
+                        let w = w as isize - 1;
+                        coordinates = Coordinates { w, x, y, z };
+                        if let Some(cube) = self.get_change(&coordinates) {
+                            changes.push(CubeUpdate { coordinates, cube })
+                        }
                     }
                 }
             }
@@ -93,14 +105,25 @@ impl PocketDimension {
         let size_z = self.cubes.len();
         let size_y = self.cubes[0].len();
         let size_x = self.cubes[0][0].len();
+        let size_w = self.cubes[0][0][0].len();
         let Coordinates {
+            w: w_offset,
             x: x_offset,
             y: y_offset,
             z: z_offset,
         } = self.offsets;
 
         // Grow
-        let (mut min_z, mut max_z, mut min_y, mut max_y, mut min_x, mut max_x) = (0, 0, 0, 0, 0, 0);
+        let (
+            mut min_z,
+            mut max_z,
+            mut min_y,
+            mut max_y,
+            mut min_x,
+            mut max_x,
+            mut min_w,
+            mut max_w,
+        ) = (0, 0, 0, 0, 0, 0, 0, 0);
         for change in changes {
             if change.coordinates.z < 0 {
                 min_z = change.coordinates.z;
@@ -120,30 +143,37 @@ impl PocketDimension {
             if change.coordinates.x > max_x {
                 max_x = change.coordinates.x;
             }
+            if change.coordinates.w < min_w {
+                min_w = change.coordinates.w;
+            }
+            if change.coordinates.w > max_w {
+                max_w = change.coordinates.w;
+            }
         }
         if min_z < 0 {
             self.cubes
-                .insert(0, vec![vec![Cube::Inactive; size_x]; size_y]);
+                .insert(0, vec![vec![vec![Cube::Inactive; size_w]; size_x]; size_y]);
             self.offsets.z += 1;
         }
         if max_z >= (size_z as isize - z_offset) {
-            self.cubes.push(vec![vec![Cube::Inactive; size_x]; size_y]);
+            self.cubes
+                .push(vec![vec![vec![Cube::Inactive; size_w]; size_x]; size_y]);
         }
         if min_y < 0 {
             for i in 0..self.cubes.len() as usize {
-                self.cubes[i].insert(0, vec![Cube::Inactive; size_x]);
+                self.cubes[i].insert(0, vec![vec![Cube::Inactive; size_w]; size_x]);
             }
             self.offsets.y += 1;
         }
         if max_y >= (size_y as isize - y_offset) {
             for i in 0..self.cubes.len() {
-                self.cubes[i].push(vec![Cube::Inactive; size_x]);
+                self.cubes[i].push(vec![vec![Cube::Inactive; size_w]; size_x]);
             }
         }
         if min_x < 0 {
             for i in 0..self.cubes.len() {
                 for j in 0..self.cubes[i].len() {
-                    self.cubes[i][j].insert(0, Cube::Inactive);
+                    self.cubes[i][j].insert(0, vec![Cube::Inactive; size_w]);
                 }
             }
             self.offsets.x += 1;
@@ -151,18 +181,42 @@ impl PocketDimension {
         if max_x >= (size_x as isize - x_offset) {
             for i in 0..self.cubes.len() {
                 for j in 0..self.cubes[i].len() {
-                    self.cubes[i][j].push(Cube::Inactive);
+                    self.cubes[i][j].push(vec![Cube::Inactive; size_w]);
+                }
+            }
+        }
+        if min_w < 0 {
+            for i in 0..self.cubes.len() {
+                for j in 0..self.cubes[i].len() {
+                    for k in 0..self.cubes[i][j].len() {
+                        self.cubes[i][j][k].insert(0, Cube::Inactive);
+                    }
+                }
+            }
+            self.offsets.w += 1;
+        }
+        if max_w >= (size_w as isize - w_offset) {
+            for i in 0..self.cubes.len() {
+                for j in 0..self.cubes[i].len() {
+                    for k in 0..self.cubes[i][j].len() {
+                        self.cubes[i][j][k].push(Cube::Inactive);
+                    }
                 }
             }
         }
 
         // Update
         for change in changes {
-            let (x, y, z) = self.shift(&change.coordinates);
-            self.cubes[z][y][x] = change.cube;
+            let (w, x, y, z) = self.shift(&change.coordinates);
+            self.cubes[z][y][x][w] = change.cube;
         }
         // Reset offsets
-        self.offsets = Coordinates { x: 0, y: 0, z: 0 };
+        self.offsets = Coordinates {
+            w: 0,
+            x: 0,
+            y: 0,
+            z: 0,
+        };
     }
 
     fn cycle(&mut self) {
@@ -170,30 +224,15 @@ impl PocketDimension {
         self.apply_changes(&changes);
     }
 
-    fn print(&self) {
-        for z in 0..self.cubes.len() {
-            println!("z = {}", z as isize - (self.cubes.len() as isize / 2));
-            for y in 0..self.cubes[z].len() {
-                for x in 0..self.cubes[z][y].len() {
-                    let c = match self.cubes[z][y][x] {
-                        Cube::Inactive => ".",
-                        Cube::Active => "#",
-                    };
-                    print!("{}", c);
-                }
-                println!();
-            }
-            println!();
-        }
-    }
-
     fn count_active(&self) -> usize {
         let mut total = 0;
         for z in 0..self.cubes.len() {
             for y in 0..self.cubes[z].len() {
                 for x in 0..self.cubes[z][y].len() {
-                    if matches!(self.cubes[z][y][x], Cube::Active) {
-                        total += 1;
+                    for w in 0..self.cubes[z][y][x].len() {
+                        if matches!(self.cubes[z][y][x][w], Cube::Active) {
+                            total += 1;
+                        }
                     }
                 }
             }
@@ -219,23 +258,31 @@ fn load_data() -> PocketDimension {
         ys.push(cubes);
     }
     PocketDimension {
-        cubes: vec![ys],
-        offsets: Coordinates { x: 0, y: 0, z: 0 },
+        cubes: vec![vec![ys]],
+        offsets: Coordinates {
+            w: 0,
+            x: 0,
+            y: 0,
+            z: 0,
+        },
     }
 }
 
 pub fn part1() {
+    // let mut pocket_dimension = load_data();
+    // for cycle in 1..7 {
+    //     pocket_dimension.cycle();
+    //     // println!("After {} cycle(s):", cycle);
+    //     // pocket_dimension.print();
+    //     // println!();
+    // }
+    // println!("Answer: {}", pocket_dimension.count_active());
+}
+
+pub fn part2() {
     let mut pocket_dimension = load_data();
-    println!("Original state:");
-    pocket_dimension.print();
-    println!();
-    for cycle in 1..7 {
+    for _ in 1..7 {
         pocket_dimension.cycle();
-        // println!("After {} cycle(s):", cycle);
-        // pocket_dimension.print();
-        // println!();
     }
     println!("Answer: {}", pocket_dimension.count_active());
 }
-
-pub fn part2() {}
