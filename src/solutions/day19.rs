f01c852c5e9d8121f2abcefa837a53e9802c2694
malorisdead <malorisdead@gmail.com>
2020-12-20 12:27:25 -0800

    Day 19 part 2 done.

--- src/solutions/day19.rs
+++ src/solutions/day19.rs
@@ -8,10 +8,8 @@ use std::fs;
 #[derive(Clone, Debug)]
 enum Rule {
     Char(char),
-    Ref(Node),
-    Double(Node, Node),
-    Triple(Node, Node, Node),
-    Or(Node, Node),
+    Ref(Vec<usize>),
+    Or(Box<Rule>, Box<Rule>),
 }
 impl std::fmt::Display for Rule {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
@@ -20,86 +18,62 @@ impl std::fmt::Display for Rule {
             "{}",
             match self {
                 Rule::Char(c) => c.to_string(),
-                Rule::Ref(r) => r.to_string(),
-                Rule::Double(one, two) => format!("{} {}", one, two),
-                Rule::Triple(one, two, three) => format!("{} {} {}", one, two, three),
+                Rule::Ref(r) => format!("{:?}", r),
                 Rule::Or(left, right) => format!("{} | {}", left, right),
             }
         )
     }
 }
 
-type Node = Box<Rule>;
-impl Validator for Node {
-    fn is_valid(&self, message: &str) -> bool {
-        match evaluate(self.to_owned(), &message.chars().collect(), 0) {
-            None => false,
-            Some(valid_chars) => valid_chars == message.len(),
-        }
-    }
-}
-trait Validator {
-    fn is_valid(&self, message: &str) -> bool;
+type Ruleset = HashMap<usize, Rule>;
+
+fn is_valid(rule: &Rule, message: &str, ruleset: &Ruleset) -> bool {
+    println!();
+    println!("Testing {}...", message);
+    let chunks = evaluate(rule, &message.chars().collect(), 0, ruleset);
+    return chunks.contains(&message.len());
 }
 
-fn evaluate(r: Node, chars: &Vec<char>, start: usize) -> Option<usize> {
+fn evaluate(r: &Rule, chars: &Vec<char>, start: usize, ruleset: &Ruleset) -> Vec<usize> {
+    let mut result = vec![];
     if start >= chars.len() {
-        return None;
+        return result;
     }
     let i = start;
-    let mut result = None;
-    // let temp = r.clone();
-    match *r {
+    let temp = r.clone();
+    match r {
         Rule::Char(c) => {
-            if c == chars[i] {
-                result = Some(1);
+            if c == &chars[i] {
+                result.push(1);
             }
         }
-        Rule::Ref(r) => result = evaluate(r, chars, i),
-        Rule::Or(l, r) => {
-            if let res @ Some(_) = evaluate(l, chars, i) {
-                result = res;
-            } else if let res @ Some(_) = evaluate(r, chars, i) {
-                result = res;
+        Rule::Ref(rules) => {
+            let mut offsets: Vec<Vec<usize>> = vec![vec![]; rules.len() + 1];
+            offsets[0].push(0);
+            for (idx, rule) in rules.iter().enumerate() {
+                for o in offsets[idx].clone() {
+                    let ri = evaluate(ruleset.get(rule).unwrap(), chars, i + o, ruleset);
+                    offsets[idx + 1].extend(ri.iter().map(|x| x + o));
+                }
             }
+            result = offsets[rules.len()].clone();
         }
-        Rule::Double(one, two) => {
-            let mut matched = 0;
-            for rule in &[one, two] {
-                matched += match evaluate(rule.to_owned(), chars, i + matched) {
-                    Some(c) => c,
-                    None => {
-                        result = None;
-                        break;
-                    }
-                };
-                result = Some(matched);
-            }
-        }
-        Rule::Triple(one, two, three) => {
-            let mut matched = 0;
-            for rule in &[one, two, three] {
-                matched += match evaluate(rule.to_owned(), chars, i + matched) {
-                    Some(c) => c,
-                    None => {
-                        result = None;
-                        break;
-                    }
-                };
-                result = Some(matched);
-            }
+        Rule::Or(left, right) => {
+            result.extend(evaluate(left, chars, i, ruleset));
+            result.extend(evaluate(right, chars, i, ruleset));
         }
     }
-    // println!("{:indent$}{}", "", matches!(result, Some(_)), indent = i);
-    // if result.is_some() {
-    //     println!(
-    //         "{:indent$}Rule {} matches {} chars",
-    //         "",
-    //         temp,
-    //         result?,
-    //         indent = i
-    //     );
-    // }
+    if result.len() > 0 {
+        println!(
+            "{:indent$}Rule {} matches {:?} chars",
+            "",
+            temp,
+            result,
+            indent = i
+        );
+    } else {
+        println!("{:indent$}Rule {} does not match", "", temp, indent = i);
+    }
     result
 }
 
@@ -127,12 +101,15 @@ fn load_data() -> (HashMap<usize, String>, Vec<String>) {
     (rules, messages)
 }
 
-fn get_rule_by_index(idx: usize, rules: &HashMap<usize, String>) -> Option<Node> {
-    let text = &rules.get(&idx)?;
-    return get_rule(text, rules);
+fn build_ruleset(rules: &HashMap<usize, String>) -> Option<Ruleset> {
+    let mut ruleset = HashMap::new();
+    for (idx, rule) in rules {
+        ruleset.insert(*idx, get_rule(rule)?);
+    }
+    Some(ruleset)
 }
 
-fn get_rule(text: &str, rules: &HashMap<usize, String>) -> Option<Node> {
+fn get_rule(text: &str) -> Option<Rule> {
     lazy_static! {
         static ref RULE_RE: Regex = Regex::new(
             r#"^(?:"(.)"|(\d+)|(?:(\d+) (\d+))|(?:(\d+) (\d+) (\d+))|(?:(.+) \| (.+)))$"#
@@ -144,44 +121,48 @@ fn get_rule(text: &str, rules: &HashMap<usize, String>) -> Option<Node> {
         .expect(&format!("Invalid rule expression: {}", text));
     if cap.get(1).is_some() {
         let c = cap[1].chars().next()?;
-        return Some(Box::new(Rule::Char(c)));
+        return Some(Rule::Char(c));
     } else if cap.get(2).is_some() {
         let i = cap[2].parse::<usize>().unwrap();
-        let node = get_rule_by_index(i, rules).expect(&format!("Invalid index in rule: {}", text));
-        return Some(Box::new(Rule::Ref(node)));
+        return Some(Rule::Ref(vec![i]));
     } else if cap.get(3).is_some() {
         let one = cap[3].parse::<usize>().unwrap();
         let two = cap[4].parse::<usize>().unwrap();
-        let one =
-            get_rule_by_index(one, rules).expect(&format!("Invalid first index in rule: {}", text));
-        let two = get_rule_by_index(two, rules)
-            .expect(&format!("Invalid second index in rule: {}", text));
-        return Some(Box::new(Rule::Double(one, two)));
+        return Some(Rule::Ref(vec![one, two]));
     } else if cap.get(5).is_some() {
         let one = cap[5].parse::<usize>().unwrap();
         let two = cap[6].parse::<usize>().unwrap();
         let three = cap[7].parse::<usize>().unwrap();
-        let one =
-            get_rule_by_index(one, rules).expect(&format!("Invalid first index in rule: {}", text));
-        let two = get_rule_by_index(two, rules)
-            .expect(&format!("Invalid second index in rule: {}", text));
-        let three = get_rule_by_index(three, rules)
-            .expect(&format!("Invalid third index in rule: {}", text));
-        return Some(Box::new(Rule::Triple(one, two, three)));
+        return Some(Rule::Ref(vec![one, two, three]));
     } else if cap.get(8).is_some() {
-        let left = get_rule(&cap[8], rules)
-            .expect(&format!("Invalid left-side expression in rule: {}", text));
-        let right = get_rule(&cap[9], rules)
-            .expect(&format!("Invalid right-side expression in rule: {}", text));
-        return Some(Box::new(Rule::Or(left, right)));
+        let left =
+            get_rule(&cap[8]).expect(&format!("Invalid left-side expression in rule: {}", text));
+        let right =
+            get_rule(&cap[9]).expect(&format!("Invalid right-side expression in rule: {}", text));
+        return Some(Rule::Or(Box::new(left), Box::new(right)));
     }
     None
 }
 
 pub fn part1() {
     let (rules, messages) = load_data();
-    let root = get_rule_by_index(0, &rules).expect("No rule found!");
-    let valid = messages.iter().filter(|m| root.is_valid(m)).count();
+    let ruleset = build_ruleset(&rules).unwrap();
+    let root = ruleset.get(&0).unwrap();
+    let valid = messages
+        .iter()
+        .filter(|m| is_valid(root, m, &ruleset))
+        .count();
     println!("Valid messages: {}", valid);
 }
-pub fn part2() {}
+pub fn part2() {
+    let (mut rules, messages) = load_data();
+    rules.insert(8, "42 | 42 8".to_string());
+    rules.insert(11, "42 31 | 42 11 31".to_string());
+    let ruleset = build_ruleset(&rules).unwrap();
+    let root = ruleset.get(&0).unwrap();
+    let valid = messages
+        .iter()
+        .filter(|m| is_valid(root, m, &ruleset))
+        .count();
+    println!("Valid messages: {}", valid);
+}
