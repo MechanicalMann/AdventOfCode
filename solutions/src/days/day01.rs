f712ef69718de4667dfb655e34629a08376d3f9a
malorisdead <malorisdead@gmail.com>
2021-12-01 10:28:58 -0800

    Day 1 part 2 done

--- solutions/src/days/day01.rs
+++ solutions/src/days/day01.rs
@@ -7,9 +7,17 @@ pub mod part1 {
     use super::*;
 
     pub fn solve() -> Result<i32> {
-        let input = AdventInput::for_day(DAY);
-        let data = input.get_lines::<i32>()?;
+        let data = AdventInput::for_day(DAY).get_lines::<i32>()?;
         count_increments(&data)
+    }
+}
+
+pub mod part2 {
+    use super::*;
+
+    pub fn solve() -> Result<i32> {
+        let data = AdventInput::for_day(DAY).get_lines::<i32>()?;
+        count_window_increments(&data, 3)
     }
 }
 
@@ -27,6 +35,24 @@ fn count_increments(numbers: &Vec<i32>) -> Result<i32> {
     Ok(incr)
 }
 
+fn count_window_increments(numbers: &Vec<i32>, window: usize) -> Result<i32> {
+    if numbers.len() < window {
+        panic!("Invalid input: window larger than input vector")
+    }
+    let mut incr = 0;
+    let mut last_sum: Option<i32> = None;
+    for start in 0..(numbers.len() - (window - 1)) {
+        let depth: i32 = numbers[start..(start + window)].iter().sum();
+        if let Some(prev) = last_sum {
+            if prev < depth {
+                incr += 1;
+            }
+        }
+        last_sum = Some(depth);
+    }
+    Ok(incr)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,5 +61,12 @@ mod tests {
         let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
         let result = count_increments(&input).unwrap();
         assert_eq!(7, result);
+    }
+
+    #[test]
+    fn should_count_window_incr() {
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        let result = count_window_increments(&input, 3).unwrap();
+        assert_eq!(5, result);
     }
 }
--- solutions/src/main.rs
+++ solutions/src/main.rs
@@ -3,4 +3,5 @@ mod input;
 
 fn main() {
     println!("Day {:02} Part {:02}: {}",  1, 1, days::day01::part1::solve().unwrap());
+    println!("Day {:02} Part {:02}: {}",  1, 2, days::day01::part2::solve().unwrap());
 }
