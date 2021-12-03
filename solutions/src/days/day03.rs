a8ef9afc9699c6ee0aa2344327b56d0bdaa4c519
malorisdead <malorisdead@gmail.com>
2021-12-03 02:18:00 -0800

    Day 3 part 2 done... but at what cost?

--- solutions/src/days/day03.rs
+++ solutions/src/days/day03.rs
@@ -15,14 +15,15 @@ pub mod part1 {
 
 pub mod part2 {
     use super::*;
-    pub fn solve() -> Result<isize> {
-        Ok(0)
+    pub fn solve() -> Result<usize> {
+        let data = AdventInput::for_day(DAY).get_lines()?;
+        get_life_support(&data)
     }
 }
 
 // This is one of those where it feels like there's a really mathy solution
 // based on fun properties of boolean logic.  But fuck that noise.
-fn count_the_ones(input: &Vec<String>) -> Result<Vec<u32>> {
+fn count_the_ones(input: &Vec<&String>) -> Result<Vec<u32>> {
     Ok(input
         .iter()
         .map(|s| {
@@ -36,14 +37,14 @@ fn count_the_ones(input: &Vec<String>) -> Result<Vec<u32>> {
 
 fn get_average_bitmask(bits: &Vec<String>) -> Result<String> {
     let total = bits.len();
-    let counts = count_the_ones(&bits)?;
-    println!("{:?}", counts);
+    let counts = count_the_ones(&bits.iter().collect())?;
     Ok(counts
         .iter()
         .map(|&c| if c as usize > total / 2 { '1' } else { '0' })
         .collect())
 }
 
+// Well this part would've been easier if I'd used bits/bools, huh
 fn get_gamma_from_bits(bits: &String) -> Result<usize, ParseIntError> {
     usize::from_str_radix(bits, 2)
 }
@@ -62,6 +63,48 @@ fn get_power_consumption(data: &Vec<String>) -> Result<usize> {
     let gamma = get_gamma_from_bits(&bits)?;
     let epsilon = get_epsilon_from_bits(&bits)?;
     Ok(gamma * epsilon)
+}
+
+enum Direction {
+    MoreCommon,
+    LessCommon,
+}
+
+fn get_rating(data: &Vec<String>, dir: Direction) -> Result<usize> {
+    let digits = data[0].len();
+    let mut values: Vec<_> = data.iter().collect();
+    for i in 0..digits {
+        let counts = count_the_ones(&values)?;
+        let midpoint = values.len() as f32 / 2.0;
+        let bit = match (&dir, counts[i] as f32 >= midpoint) {
+            (Direction::MoreCommon, true) => '1',
+            (Direction::MoreCommon, false) => '0',
+            (Direction::LessCommon, true) => '0',
+            (Direction::LessCommon, false) => '1',
+        };
+        values = values
+            .iter()
+            .filter(|s| s.chars().nth(i).unwrap() == bit)
+            .map(|&s| s)
+            .collect();
+        if values.len() == 1 {
+            break;
+        }
+    }
+    let val = usize::from_str_radix(values[0], 2)?;
+    Ok(val)
+}
+
+fn get_oxygen(data: &Vec<String>) -> Result<usize> {
+    get_rating(&data, Direction::MoreCommon)
+}
+
+fn get_co2(data: &Vec<String>) -> Result<usize> {
+    get_rating(&data, Direction::LessCommon)
+}
+
+fn get_life_support(data: &Vec<String>) -> Result<usize> {
+    Ok(get_oxygen(&data)? * get_co2(&data)?)
 }
 
 #[cfg(test)]
@@ -112,5 +155,32 @@ mod tests {
         .collect();
         let power = get_power_consumption(&data).unwrap();
         assert_eq!(198, power)
+    }
+
+    #[test]
+    fn should_filter_more_common() {
+        let data = vec!["01".to_owned(), "10".to_owned(), "00".to_owned()];
+        let rating = get_rating(&data, Direction::MoreCommon).unwrap();
+        assert_eq!(1, rating)
+    }
+
+    #[test]
+    fn should_filter_less_common() {
+        let data = vec!["01".to_owned(), "10".to_owned(), "00".to_owned()];
+        let rating = get_rating(&data, Direction::LessCommon).unwrap();
+        assert_eq!(2, rating)
+    }
+
+    #[test]
+    fn should_get_life_support_rating() {
+        let data = vec![
+            "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000",
+            "11001", "00010", "01010",
+        ]
+        .iter()
+        .map(|&s| s.to_owned())
+        .collect();
+        let rating = get_life_support(&data).unwrap();
+        assert_eq!(230, rating)
     }
 }
