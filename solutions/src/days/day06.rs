3daaf0d89af3b7c4b6c90355e36f979417341e03
malorisdead <malorisdead@gmail.com>
2021-12-06 00:55:48 -0800

    Day 6 part 2 done

--- solutions/src/days/day06.rs
+++ solutions/src/days/day06.rs
@@ -1,5 +1,6 @@
 use crate::input::AdventInput;
 use anyhow::Result;
+use std::collections::HashMap;
 
 const DAY: u8 = 6;
 
@@ -8,7 +9,7 @@ pub mod part1 {
 
     pub fn solve() -> Result<usize> {
         let mut data = AdventInput::for_day(DAY).get_csv_as::<u8>()?;
-        game_of_fish(&mut data, 80)?;
+        game_of_fish(&mut data, 80);
         Ok(data.len())
     }
 }
@@ -17,8 +18,8 @@ pub mod part2 {
     use super::*;
 
     pub fn solve() -> Result<usize> {
-        let mut data = AdventInput::for_day(DAY).get_csv_as::<u8>()?;
-        Ok(0)
+        let data = AdventInput::for_day(DAY).get_csv_as::<u8>()?;
+        gonna_need_a_bigger_boat(&data, 256)
     }
 }
 
@@ -35,12 +36,29 @@ fn tick(fish: &mut Vec<u8>) {
     fish.extend(vec![8; spawn]);
 }
 
-fn game_of_fish(fish: &mut Vec<u8>, iterations: usize) -> Result<usize> {
+fn game_of_fish(fish: &mut Vec<u8>, iterations: usize) {
     for _i in 1..=iterations {
         tick(fish);
-        println!("After {:2} days: {:?}", _i, fish.len());
+        // println!("After {:2} days: {:?}", _i, fish.len());
     }
-    Ok(0)
+}
+
+fn gonna_need_a_bigger_boat(fish: &[u8], iterations: usize) -> Result<usize> {
+    let mut tracker: HashMap<u8, usize> = HashMap::from_iter((0u8..=8u8).map(|v| (v, 0)));
+    for &f in fish {
+        let count = tracker.get_mut(&f).unwrap();
+        *count += 1;
+    }
+    for _i in 1..=iterations {
+        let spawn = tracker[&0];
+        for j in 1..=8 {
+            tracker.insert(j - 1, tracker[&j]);
+        }
+        tracker.insert(6, tracker[&6] + spawn);
+        tracker.insert(8, spawn);
+        // println!("After {:3} days: {}", _i, tracker.values().sum::<usize>());
+    }
+    Ok(tracker.values().sum())
 }
 
 #[cfg(test)]
@@ -68,15 +86,22 @@ mod tests {
     }
 
     #[test]
-    fn should_solve_example() -> Result<()> {
+    fn should_solve_example() {
         let mut fish = vec![3, 4, 3, 1, 2];
-        game_of_fish(&mut fish, 18)?;
+        game_of_fish(&mut fish, 18);
         let expected = vec![
             6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8,
         ];
         for (&e, a) in expected.iter().zip(fish) {
             assert_eq!(e, a);
         }
+    }
+
+    #[test]
+    fn should_solve_part2_example() -> Result<()> {
+        let fish = vec![3, 4, 3, 1, 2];
+        let count = gonna_need_a_bigger_boat(&fish, 256)?;
+        assert_eq!(26984457539, count);
         Ok(())
     }
 }
