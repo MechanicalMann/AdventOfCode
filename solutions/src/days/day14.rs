a156b27de3d303946f8d2674fa6d267bd4b0c344
Michael Mechmann <malorisdead@gmail.com>
2021-12-14 13:56:16 -0800

    Day 14 part 2 done

--- solutions/src/days/day14.rs
+++ solutions/src/days/day14.rs
@@ -19,12 +19,15 @@ impl Solver<usize, usize> for Solution {
     }
 
     fn part_two(&self) -> anyhow::Result<usize> {
-        Ok(0)
+        let mut polymer = self.input().get_as::<Polymer>()?;
+        polymer.step_for(40);
+        Ok(polymer.get_range())
     }
 }
 
 struct Polymer {
-    elements: Vec<char>,
+    pairs: HashMap<(char, char), usize>,
+    counts: HashMap<char, usize>,
     insertions: HashMap<(char, char), char>,
 }
 impl FromStr for Polymer {
@@ -32,7 +35,17 @@ impl FromStr for Polymer {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut spl = s.split("\n\n");
+
         let elements = spl.next().unwrap().chars().collect_vec();
+        let mut pairs: HashMap<(char, char), usize> = HashMap::new();
+        let mut counts: HashMap<char, usize> = HashMap::new();
+        for i in 0..(elements.len() - 1) {
+            let pair = (elements[i], elements[i + 1]);
+            *pairs.entry(pair).or_insert(0) += 1;
+            *counts.entry(pair.0).or_insert(0) += 1;
+        }
+        *counts.entry(elements[elements.len() - 1]).or_insert(0) += 1;
+
         let mut insertions = HashMap::new();
         for line in spl.next().unwrap().lines() {
             let mut ins = line.split(" -> ");
@@ -41,43 +54,38 @@ impl FromStr for Polymer {
             insertions.insert(pair, c);
         }
         Ok(Polymer {
-            elements,
+            pairs,
+            counts,
             insertions,
         })
     }
 }
 impl Polymer {
     fn step(&mut self) {
-        let mut to_insert: Vec<(usize, char)> = vec![];
-        for i in 0..(self.elements.len() - 1) {
-            let a = self.elements[i];
-            let b = self.elements[i + 1];
-            if let Some(&c) = self.insertions.get(&(a, b)) {
-                to_insert.push((i + 1, c));
+        let mut newpairs = self.pairs.clone();
+        for (&pair, &c) in &self.insertions {
+            if let Some(&count) = self.pairs.get(&pair) {
+                if count == 0 {
+                    continue;
+                }
+                for np in [(pair.0, c), (c, pair.1)] {
+                    *newpairs.entry(np).or_insert(0) += count;
+                }
+                *self.counts.entry(c).or_insert(0) += count;
+                *newpairs.get_mut(&pair).unwrap() -= count;
             }
         }
-        for (i, &(pos, c)) in to_insert.iter().enumerate() {
-            self.elements.insert(pos + i, c);
-        }
+        self.pairs = newpairs;
     }
 
     fn step_for(&mut self, iterations: usize) {
-        for _ in 0..iterations {
+        for _i in 0..iterations {
             self.step();
         }
     }
 
-    fn count(&self) -> HashMap<char, usize> {
-        let mut counts: HashMap<char, usize> = HashMap::new();
-        for e in &self.elements {
-            *counts.entry(*e).or_insert(0) += 1;
-        }
-        counts
-    }
-
     fn get_range(&self) -> usize {
-        let counts = self.count();
-        if let MinMax(min, max) = counts.iter().minmax_by(|&a, &b| a.1.cmp(&b.1)) {
+        if let MinMax(min, max) = self.counts.iter().minmax_by(|&a, &b| a.1.cmp(&b.1)) {
             return max.1 - min.1;
         }
         0
@@ -88,19 +96,28 @@ impl Polymer {
 mod tests {
     use super::*;
 
+    impl Polymer {
+        fn len(&self) -> usize {
+            self.counts.values().sum()
+        }
+    }
+
     #[test]
     fn should_parse() {
         let input = "AB
 
 AB -> C";
         let polymer = input.parse::<Polymer>().unwrap();
-        assert_eq!(2, polymer.elements.len());
-        assert_eq!(vec!['A', 'B'], polymer.elements);
+        assert_eq!(1, polymer.pairs.len());
+        assert_eq!(vec![&('A', 'B')], polymer.pairs.keys().collect_vec());
+        assert_eq!(1, polymer.pairs[&('A', 'B')]);
         assert_eq!(1, polymer.insertions.len());
         assert_eq!(
             vec![(('A', 'B'), 'C')],
             polymer.insertions.into_iter().collect_vec()
         );
+        assert_eq!(1, polymer.counts[&'A']);
+        assert_eq!(1, polymer.counts[&'B']);
     }
 
     #[test]
@@ -110,8 +127,46 @@ AB -> C";
 AB -> C";
         let mut polymer = input.parse::<Polymer>().unwrap();
         polymer.step();
-        assert_eq!(3, polymer.elements.len());
-        assert_eq!(vec!['A', 'C', 'B'], polymer.elements);
+        assert_eq!(3, polymer.pairs.len());
+        assert_eq!(
+            vec![&('A', 'B'), &('A', 'C'), &('C', 'B')],
+            polymer.pairs.keys().sorted().collect_vec()
+        );
+        assert_eq!(0, polymer.pairs[&('A', 'B')]);
+        assert_eq!(1, polymer.pairs[&('A', 'C')]);
+        assert_eq!(1, polymer.pairs[&('C', 'B')]);
+        assert_eq!(1, polymer.counts[&'A']);
+        assert_eq!(1, polymer.counts[&'B']);
+        assert_eq!(1, polymer.counts[&'C']);
+    }
+
+    #[test]
+    fn should_keep_count() {
+        let input = "NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+        let mut polymer = input.parse::<Polymer>().unwrap();
+        polymer.step();
+        assert_eq!(7, polymer.len());
+        polymer.step();
+        assert_eq!(13, polymer.len());
+        polymer.step();
+        assert_eq!(25, polymer.len());
     }
 
     #[test]
@@ -136,15 +191,44 @@ CC -> N
 CN -> C";
         let mut polymer = input.parse::<Polymer>().unwrap();
         polymer.step_for(10);
-        assert_eq!(3073, polymer.elements.len());
+        assert_eq!(1749, polymer.counts[&'B']);
+        assert_eq!(298, polymer.counts[&'C']);
+        assert_eq!(161, polymer.counts[&'H']);
+        assert_eq!(865, polymer.counts[&'N']);
 
-        let counts = polymer.count();
-        assert_eq!(1749, counts[&'B']);
-        assert_eq!(298, counts[&'C']);
-        assert_eq!(161, counts[&'H']);
-        assert_eq!(865, counts[&'N']);
+        assert_eq!(3073, polymer.len());
 
         let range = polymer.get_range();
         assert_eq!(1588, range);
+    }
+
+    #[test]
+    fn should_solve_part2_example() {
+        let input = "NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+        let mut polymer = input.parse::<Polymer>().unwrap();
+        polymer.step_for(40);
+
+        assert_eq!(2192039569602, polymer.counts[&'B']);
+        assert_eq!(3849876073, polymer.counts[&'H']);
+
+        let range = polymer.get_range();
+        assert_eq!(2188189693529, range);
     }
 }
