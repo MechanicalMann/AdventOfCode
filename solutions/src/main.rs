f3f1082ef984e586e0a80a497ae94b18b2c235cf
Michael Mechmann <malorisdead@gmail.com>
2021-12-15 23:15:32 -0800

    Hack together some performance analysis

--- solutions/src/main.rs
+++ solutions/src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
-use solver::Solver;
+use itertools::Itertools;
+use prettytable::{cell, color, format::Alignment, row, Attr, Cell, Row, Table};
+use solver::{Measure, Solver};
 
 mod days;
 mod input;
@@ -21,5 +23,125 @@ fn main() -> Result<()> {
     days::day13::Solution::new().solve()?;
     days::day14::Solution::new().solve()?;
     days::day15::Solution::new().solve()?;
+
+const TITLES: [&str; 25] = [
+    "Sonar Sweep",
+    "Dive!",
+    "Binary Diagnostic",
+    "Giant Squid",
+    "Hydrothermal Venture",
+    "Lanternfish",
+    "The Treachery of Whales",
+    "Seven Segment Search",
+    "Smoke Basin",
+    "Syntax Scoring",
+    "Dumbo Octopus",
+    "Passage Pathing",
+    "Transparent Origami",
+    "Extended Polymerization",
+    "Chiton",
+    "???",
+    "???",
+    "???",
+    "???",
+    "???",
+    "???",
+    "???",
+    "???",
+    "???",
+    "???",
+];
+
+fn perf() -> Result<()> {
+    let measures: Vec<_> = vec![
+        Measure::get(days::day01::Solution::new()),
+        Measure::get(days::day02::Solution::new()),
+        Measure::get(days::day03::Solution::new()),
+        Measure::get(days::day04::Solution::new()),
+        Measure::get(days::day05::Solution::new()),
+        Measure::get(days::day06::Solution::new()),
+        Measure::get(days::day07::Solution::new()),
+        Measure::get(days::day08::Solution::new()),
+        Measure::get(days::day09::Solution::new()),
+        Measure::get(days::day10::Solution::new()),
+        Measure::get(days::day11::Solution::new()),
+        Measure::get(days::day12::Solution::new()),
+        Measure::get(days::day13::Solution::new()),
+        Measure::get(days::day14::Solution::new()),
+        Measure::get(days::day15::Solution::new()),
+    ];
+    let mut results = vec![];
+    for m in measures {
+        results.push((m.describe_part_one(), m.time_part_one()?));
+        results.push((m.describe_part_two(), m.time_part_two()?));
+    }
+
+    // Get some rough stats
+    let len = results.len();
+    let (i1, i2, i3) = (len / 2, (len / 4) * 2, (len * 90) / 100);
+    let ranked = results
+        .iter()
+        .sorted_by(|a, b| a.1.cmp(&b.1))
+        .map(|(_, d)| d.as_secs_f32())
+        .collect_vec();
+    let (p1, p2, p3) = (ranked[i1], ranked[i2], ranked[i3]);
+
+    let get_quartile_color = |d: f32| match d {
+        _ if d < p1 => color::GREEN,
+        _ if d >= p1 && d < p2 => color::BRIGHT_GREEN,
+        _ if d >= p2 && d < p3 => color::YELLOW,
+        _ => color::BRIGHT_RED,
+    };
+
+    let total = ranked.iter().sum::<f32>();
+    let mean = total / (len as f32);
+    let median = ranked[i2];
+
+    let mut table = Table::new();
+    table.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+
+    table.set_titles(row!["Year", "Day", "Part", "Title", "Time"]);
+
+    for (key, res) in results {
+        let s = key.split(' ').collect_vec();
+        let (day, part) = (s[1], s[3]);
+        let title = TITLES[(day.parse::<usize>().unwrap() - 1)];
+        let dur = res.as_secs_f32();
+        table.add_row(Row::new(vec![
+            Cell::new("2021"),
+            Cell::new(day),
+            Cell::new(part),
+            Cell::new(title),
+            Cell::new(&format!("{:0.04}s", dur))
+                .with_style(Attr::ForegroundColor(get_quartile_color(dur))),
+        ]));
+    }
+
+    table.add_row(Row::new(vec![Cell::new("").with_hspan(5)]));
+
+    table.add_row(Row::new(vec![
+        Cell::new_align("Total", Alignment::RIGHT)
+            .with_style(Attr::Bold)
+            .with_hspan(4),
+        Cell::new(&format!("{:0.04}s", total))
+            .with_style(Attr::ForegroundColor(get_quartile_color(total))),
+    ]));
+    table.add_row(Row::new(vec![
+        Cell::new_align("Average", Alignment::RIGHT)
+            .with_style(Attr::Bold)
+            .with_hspan(4),
+        Cell::new(&format!("{:0.04}s", mean))
+            .with_style(Attr::ForegroundColor(get_quartile_color(mean))),
+    ]));
+    table.add_row(Row::new(vec![
+        Cell::new_align("Median", Alignment::RIGHT)
+            .with_style(Attr::Bold)
+            .with_hspan(4),
+        Cell::new(&format!("{:0.04}s", median))
+            .with_style(Attr::ForegroundColor(get_quartile_color(median))),
+    ]));
+
+    table.printstd();
+
     Ok(())
 }
--- solutions/src/solver.rs
+++ solutions/src/solver.rs
@@ -1,7 +1,11 @@
 use crate::input::AdventInput;
 use anyhow::Result;
-use std::fmt::Display;
-
+use gag::Gag;
+use std::{
+    fmt::Display,
+    marker::PhantomData,
+    time::{Duration, Instant},
+};
 pub trait Solver<T1: Display, T2: Display> {
     const DAY: u8;
 
@@ -20,12 +24,70 @@ pub trait Solver<T1: Display, T2: Display> {
     fn new() -> Self;
 
     fn print_part_one<T: Display>(&self, result: T) {
-        println!("Day {:02} Part 01: {}", Self::DAY, result);
+        println!("{}: {}", self.describe_part_one(), result);
     }
     fn print_part_two<T: Display>(&self, result: T) {
-        println!("Day {:02} Part 02: {}", Self::DAY, result);
+        println!("{}: {}", self.describe_part_two(), result);
     }
 
     fn part_one(&self) -> Result<T1>;
     fn part_two(&self) -> Result<T2>;
+
+    fn describe(&self) -> String {
+        format!("Day {:02}", Self::DAY)
+    }
+    fn describe_part(&self, part: isize) -> String {
+        format!("{} Part {:02}", self.describe(), part)
+    }
+    fn describe_part_one(&self) -> String {
+        self.describe_part(1)
+    }
+    fn describe_part_two(&self) -> String {
+        self.describe_part(2)
+    }
+}
+
+pub trait Measurable {
+    fn describe_part_one(&self) -> String;
+    fn describe_part_two(&self) -> String;
+    fn time_part_one(&self) -> Result<Duration>;
+    fn time_part_two(&self) -> Result<Duration>;
+}
+
+pub struct Measure<T: Solver<T1, T2>, T1: Display, T2: Display> {
+    solver: T,
+    _p1: PhantomData<T1>,
+    _p2: PhantomData<T2>,
+}
+impl<T: Solver<T1, T2>, T1: Display, T2: Display> Measurable for Measure<T, T1, T2> {
+    fn describe_part_one(&self) -> String {
+        self.solver.describe_part_one()
+    }
+    fn describe_part_two(&self) -> String {
+        self.solver.describe_part_two()
+    }
+    fn time_part_one(&self) -> Result<Duration> {
+        time_execution(|| self.solver.part_one())
+    }
+    fn time_part_two(&self) -> Result<Duration> {
+        time_execution(|| self.solver.part_two())
+    }
+}
+impl<T: 'static + Solver<T1, T2>, T1: 'static + Display, T2: 'static + Display> Measure<T, T1, T2> {
+    pub fn get(solver: T) -> Box<dyn Measurable> {
+        Box::new(Measure {
+            solver,
+            _p1: PhantomData {},
+            _p2: PhantomData {},
+        })
+    }
+}
+
+fn time_execution<F: Fn() -> Result<T>, T>(f: F) -> Result<Duration> {
+    let gag = Gag::stdout()?;
+    let now = Instant::now();
+    f()?;
+    let res = now.elapsed();
+    drop(gag);
+    Ok(res)
 }
