46d2a6233f33b6c90cc95a90ee4d007c336999b1
Michael Mechmann <m@michaelmechmann.net>
2023-12-04 00:25:17 -0800

    Day 4 part 2 done.

--- solutions/src/days/day04.rs
+++ solutions/src/days/day04.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashSet, str::FromStr};
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 
 use crate::solver::Solver;
 use anyhow::{anyhow, Result};
@@ -18,7 +21,8 @@ impl Solver<usize, usize> for Solution {
     }
 
     fn part_two(&self) -> Result<usize> {
-        Ok(0)
+        let scratchers = self.input().get_lines_as::<Scratcher>()?;
+        Ok(play_game(&scratchers))
     }
 }
 
@@ -45,12 +49,28 @@ impl FromStr for Scratcher {
 }
 impl Scratcher {
     fn score(&self) -> usize {
-        let wins = self.winners.intersection(&self.numbers).count();
+        let wins = self.get_num_matched();
         if wins == 0 {
             return 0;
         }
         2usize.pow(wins as u32 - 1)
     }
+
+    fn get_num_matched(&self) -> usize {
+        self.winners.intersection(&self.numbers).count()
+    }
+}
+
+fn play_game(scratchers: &[Scratcher]) -> usize {
+    let mut counts: HashMap<usize, usize> = HashMap::new();
+    for (i, card) in scratchers.iter().enumerate() {
+        let e = counts.entry(i).and_modify(|c| *c += 1).or_insert(1).clone();
+        for j in 1..=card.get_num_matched() {
+            let next = counts.entry(i + j).or_insert(0);
+            *next += e;
+        }
+    }
+    counts.values().sum()
 }
 
 #[cfg(test)]
@@ -93,6 +113,31 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
         let scores = scratchers.map(|s| s.score()).collect_vec();
         let total: usize = scores.iter().sum();
         assert_eq!(13, total);
+        Ok(())
+    }
+
+    #[test]
+    fn should_add_cards() -> Result<()> {
+        let test = "Game 1: 1 2 3 | 2 3 4
+Game 2: 1 2 3 | 3 4 5
+Game 3: 1 2 3 | 4 5 6";
+        let scratchers = test
+            .lines()
+            .map(|l| l.parse::<Scratcher>().unwrap())
+            .collect_vec();
+        let total = play_game(&scratchers);
+        assert_eq!(7, total);
+        Ok(())
+    }
+
+    #[test]
+    fn should_solve_part2() -> Result<()> {
+        let scratchers = EXAMPLE_INPUT
+            .lines()
+            .map(|l| l.parse::<Scratcher>().unwrap())
+            .collect_vec();
+        let total = play_game(&scratchers);
+        assert_eq!(30, total);
         Ok(())
     }
 }
