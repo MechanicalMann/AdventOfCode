2b391c64cebcc67d3b54e7216ea141604c98eae1
Michael Mechmann <m@michaelmechmann.net>
2023-12-09 13:43:24 -0800

    I gotta stop committing the commented-out lines

--- solutions/src/main.rs
+++ solutions/src/main.rs
@@ -37,12 +37,12 @@ fn main() -> Result<()> {
 }
 
 fn solve() -> Result<()> {
-    // days::day01::Solution::new().solve()?;
-    // days::day02::Solution::new().solve()?;
-    // days::day03::Solution::new().solve()?;
-    // days::day04::Solution::new().solve()?;
-    // days::day05::Solution::new().solve()?;
-    // days::day06::Solution::new().solve()?;
+    days::day01::Solution::new().solve()?;
+    days::day02::Solution::new().solve()?;
+    days::day03::Solution::new().solve()?;
+    days::day04::Solution::new().solve()?;
+    days::day05::Solution::new().solve()?;
+    days::day06::Solution::new().solve()?;
     days::day07::Solution::new().solve()?;
     days::day08::Solution::new().solve()?;
     days::day09::Solution::new().solve()?;
